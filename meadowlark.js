861532ec95c362a927e7aad510856eaa75d3d2b3
LeoLau <liuyuansix@163.com>
2018-10-24 17:58:27 +0800

    初步使用模板

--- meadowlark.js
+++ meadowlark.js
@@ -5,16 +5,18 @@ var express = require('express');
 
 var app = express();
 
+var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT|| 3000);
 
 app.get('/', function (req, res) {
-  res.type('text/plain');
-  res.send('Meadowlark Travel');
+  res.render('home');
 });
 
 app.get('/about', function (req, res) {
-  res.type('text/plain');
-  res.send('About Meadowlark Travel');
+  res.render('about');
 });
 
 app.use(function (req, res) {
