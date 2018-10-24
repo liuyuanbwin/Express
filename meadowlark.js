f413edf93aa0b7d55768f03840345c9cb743b497
LeoLau <liuyuansix@163.com>
2018-10-24 18:23:42 +0800

    加载静态文件

--- meadowlark.js
+++ meadowlark.js
@@ -11,6 +11,8 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT|| 3000);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
   res.render('home');
 });
@@ -20,16 +22,14 @@ app.get('/about', function (req, res) {
 });
 
 app.use(function (req, res) {
-  res.type('text/plain');
   res.status(404);
-  res.send('404 - Not Found');
+  res.render('404');
 });
 
 app.use(function (req, res, next) {
   console.error(err.stack);
-  res.type('text/plain');
   res.status(500);
-  res.send('500 - Server Error');
+  res.rander('500');
 });
 
 app.listen(app.get('port'), function () {
